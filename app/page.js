92f79633bc607c6a27e5aeb0424336ae5bdde9f8
Mohamed Megahed <m.megahed168@gmail.com>
2025-11-12 15:48:48 +0200

    setting things up

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 function HomePage() {
   return (
-    <h1> Thats Where Everthing Starts </h1>
-      
-    
+    <div>
+      {" "}
+      <h1 className="text-4xl font-bold"> Thats Where Everthing Starts </h1>
+      <p> And This is the P tag </p>
+      <h2> Another Headline </h2>
+      <h3> Sub Headline</h3>
+    </div>
   );
 }
 
 export default HomePage;
-
