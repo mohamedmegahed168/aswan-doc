8224d6f954358f3b86c12a121d1da999c0cea03a
Mohamed Megahed <m.megahed168@gmail.com>
2025-11-23 13:59:03 +0200

    authentication setup

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,4 +1,37 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/storage/firebase";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 function DashboardPage() {
-  return <h1>Dashboard Page</h1>;
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const userEmail = searchParams.get("user") || "Guest";
+  /*useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("Current user:", currentUser);
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);*/
+  async function handleSignOut() {
+    await signOut(auth);
+    router.push("/");
+  }
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      <p>Welcome, {userEmail}!</p>
+      <button
+        className="border border-slate-400 rounded bg-slate-200 w-24 cursor-pointer"
+        onClick={handleSignOut}
+      >
+        Sign Out
+      </button>
+    </div>
+  );
 }
 export default DashboardPage;
---
+++ app/page.jsx
@@ -0,0 +1,105 @@
+"use client";
+import {
+  collection,
+  onSnapshot,
+  setDoc,
+  doc,
+  getDoc,
+} from "firebase/firestore";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { db, auth } from "../storage/firebase";
+import { use, useEffect, useState } from "react";
+
+function HomePage() {
+  const router = useRouter();
+  const namesRefrence = collection(db, "names");
+  const [user, setUser] = useState(null);
+  async function handleSignUp(e) {
+    e.preventDefault();
+
+    const email = String(e.target.email.value || "").trim();
+    const password = String(e.target.password.value || "");
+    await createUserWithEmailAndPassword(auth, email, password);
+  }
+  async function handleSignIn(e) {
+    e.preventDefault();
+    const email = String(e.target.email.value || "").trim();
+    const password = String(e.target.password.value || "");
+    const userCredentials = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    router.push(`/dashboard?user=${userCredentials.user.email}`);
+  }
+  /* useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  });*/
+  return (
+    <div className="flex flex-col items-center gap-8 h-screen">
+      {" "}
+      <h1 className="text-4xl font-bold"> Thats Where Everthing Starts </h1>
+      <div className="border-1 border-slate-400 w-64">
+        <form
+          className="flex flex-col gap-4 w-60 items-center m-2"
+          onSubmit={handleSignUp}
+        >
+          <input
+            type="email"
+            placeholder="email"
+            className="border-1 border-slate-400"
+            name="email"
+          />
+          <input
+            type="password"
+            placeholder="password"
+            className="border-1 border-slate-400"
+            name="password"
+          />
+
+          <button
+            className="cursor-pointer block bg-slate-300 border border-slate-400 m-4"
+            type="submit"
+          >
+            Click Me to Sign Up
+          </button>
+        </form>
+      </div>
+      <div className="border-1 border-slate-400 w-64">
+        <form
+          className="flex flex-col gap-4 w-60 items-center m-2"
+          onSubmit={handleSignIn}
+        >
+          <input
+            type="email"
+            placeholder="email"
+            className="border-1 border-slate-400"
+            name="email"
+          />
+          <input
+            type="password"
+            placeholder="password"
+            className="border-1 border-slate-400"
+            name="password"
+          />
+          <button
+            className="cursor-pointer block bg-slate-300 border border-slate-400 m-4"
+            type="submit"
+          >
+            Click Me to Sign In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
